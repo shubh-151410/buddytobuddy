fcda3d8ae3bb24e8887e2c8e6a3484435b524d27
Shubhanshu <shubhanshu@sensenuts.com>
2020-01-25 20:54:59 +0530

    jk

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/shubhanshu/Desktop/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/shubhanshu/Desktop/BuddyToBuddy/buddytobuddy"
+export "FLUTTER_APPLICATION_PATH=/home/shubhanshu/Desktop/buddytobuddy"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
